a04b31a516a8320a34cdb6ed855ac768170e6ae5
MarkZither <mark.burton@zither-it.co.uk>
2024-01-05 21:18:07 +0100

    first working version of the first example on stephan toubs guide

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # My First AI Chat Bot
+
+## Working with Azure OpenAI and Semantic Kernel
+
+[Demystifying Retrieval Augmented Generation with .NET by Stephen Toub](https://devblogs.microsoft.com/dotnet/demystifying-retrieval-augmented-generation-with-dotnet/)
+
---
+++ src/Azure.OpenAI.SemanticKernel.ChatApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SemanticKernel;
+
+internal class Program {
+    private static async Task Main(string[] args) {
+        string aoaiEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")!;
+        string aoaiApiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")!;
+        string aoaiModel = "Gpt35Turbo_0301";
+        string oaiModel = "gpt-3.5-turbo-1106";
+
+        // Initialize the kernel
+        //var builder = new Kernel.Builder.;
+        IKernelBuilder builder = Kernel.CreateBuilder();
+        //builder.Services.AddAzureOpenAIChatCompletion(aoaiModel, aoaiEndpoint, aoaiApiKey);
+        builder.Services.AddOpenAIChatCompletion(oaiModel, aoaiApiKey);
+
+        var kernel = builder.Build();
+
+        // Q&A loop
+        while (true) {
+            Console.Write("Question: ");
+            Console.WriteLine((await kernel.InvokePromptAsync(Console.ReadLine()!)).GetValue<string>());
+            Console.WriteLine();
+        }
+    }
+}
